1fea367a822b02065e68a7cd9c3558d9d2db474c
DavidsonGM <davidgmendes@hotmail.com>
2023-10-29 23:45:28 -0300

    :ambulance: remove assets precompile

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,6 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
 yarn install
+bundle install
+bundle exec rake db:migrate
